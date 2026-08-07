6b906f74d9f0cdfb8db70566a04fed7e832dad6a
4k29 <2hjpkx4564@privaterelay.appleid.com>
2026-08-07 15:42:47 +0900

    Add selectable iPad Pro configurations

--- variant-overrides.js
+++ variant-overrides.js
@@ -87,9 +87,89 @@
     variant.options.storage === "1TB"
   );
 
+  const ipadImages = {
+    "シルバー": "https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/refurb-ipad-pro-13inch-8th-gen-wifi-silver-202512?wid=1200&hei=630&fmt=jpeg&qlt=95&.v=1770008685355",
+    "スペースブラック": "https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/refurb-ipad-pro-13inch-8th-gen-wifi-space-black-202512?wid=1200&hei=630&fmt=jpeg&qlt=95",
+  };
+
+  const ipadPrices = {
+    "Wi-Fi": {
+      "256GB": { "標準ガラス": 209800 },
+      "512GB": { "標準ガラス": 239800 },
+      "1TB": { "標準ガラス": 297800, "Nano-texture": 311800 },
+      "2TB": { "標準ガラス": 355800, "Nano-texture": 369800 },
+    },
+    "Wi-Fi + Cellular": {
+      "256GB": { "標準ガラス": 239800 },
+      "512GB": { "標準ガラス": 270800 },
+      "1TB": { "標準ガラス": 328800, "Nano-texture": 341800 },
+      "2TB": { "標準ガラス": 385800, "Nano-texture": 399800 },
+    },
+  };
+
+  const ipadVariants = [];
+  for (const color of colors) {
+    for (const [connection, storageOptions] of Object.entries(ipadPrices)) {
+      for (const [storage, glassOptions] of Object.entries(storageOptions)) {
+        for (const [glass, price] of Object.entries(glassOptions)) {
+          ipadVariants.push({
+            options: { color, connection, storage, glass },
+            price,
+            image: ipadImages[color],
+            configuration: `${color}・${connection}・${storage}・${glass}`,
+          });
+        }
+      }
+    }
+  }
+
+  const ipadDefaultVariant = ipadVariants.findIndex((variant) =>
+    variant.options.color === "シルバー" &&
+    variant.options.connection === "Wi-Fi" &&
+    variant.options.storage === "1TB" &&
+    variant.options.glass === "Nano-texture"
+  );
+
   window.WANT_PRODUCT_OVERRIDES = {
     "iphone-air": {
       configurationSuffix: "SIMフリー・学生・教職員向けストア",
+      url: "https://www.apple.com/jp-edu/shop/buy-iphone/iphone-air",
+    },
+    "ipad-pro-m4-refurb": {
+      url: "https://www.apple.com/jp/shop/refurbished/ipad/13インチ-13インチipad-pro",
+      defaultVariant: ipadDefaultVariant,
+      optionOrder: ["color", "connection", "storage", "glass"],
+      optionLabels: {
+        color: "カラー",
+        connection: "接続",
+        storage: "ストレージ",
+        glass: "ガラス",
+      },
+      optionColors: {
+        "シルバー": "#e3e4e1",
+        "スペースブラック": "#2c2d2f",
+      },
+      optionDetailValues: {
+        color: {
+          "シルバー": { "カラー": "シルバー" },
+          "スペースブラック": { "カラー": "スペースブラック" },
+        },
+        connection: {
+          "Wi-Fi": { "接続": "Wi-Fi" },
+          "Wi-Fi + Cellular": { "接続": "Wi-Fi + Cellular" },
+        },
+        storage: {
+          "256GB": { "ストレージ": "256GB", "メモリ": "8GB" },
+          "512GB": { "ストレージ": "512GB", "メモリ": "8GB" },
+          "1TB": { "ストレージ": "1TB", "メモリ": "16GB" },
+          "2TB": { "ストレージ": "2TB", "メモリ": "16GB" },
+        },
+        glass: {
+          "標準ガラス": { "ガラス": "標準ガラス" },
+          "Nano-texture": { "ガラス": "Nano-textureガラス" },
+        },
+      },
+      variants: ipadVariants,
     },
     "macbook-pro-14-m5-pro": {
       defaultVariant: macDefaultVariant,
